581900f48ed61ef4083574b576c5629f8b3b3615
Salikhovakbar <pcfjkjhkk@gmail.com>
2022-11-28 17:58:00 +0500

    fixed media

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -38,7 +38,7 @@ else{
         "https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg",
         "https://m.media-amazon.com/images/I/61BvxKSpy3L._SX3000_.jpg",
         "https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg",
-        "https://m.media-amazon.com/images/I/71qid7QFWJL._SX3000_.jpg",
+        "https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg",
         "https://m.media-amazon.com/images/I/61-8rBAD68L._SX3000_.jpg"
       ];
       function nextPage(){
